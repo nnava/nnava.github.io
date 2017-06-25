986c00036bfbee9505d56c3e1a5c772ee867d77e
nnava <lill4ekonomen@gmail.com>
2017-06-25 21:48:12 +0200

    Updated Telerik version and stock data

--- styles/kendo.default.mobile.min.css
+++ styles/kendo.default.mobile.min.css
@@ -1,5 +1,5 @@
 /** 
- * Kendo UI v2017.2.504 (http://www.telerik.com/kendo-ui)                                                                                                                                               
+ * Kendo UI v2017.2.621 (http://www.telerik.com/kendo-ui)                                                                                                                                               
  * Copyright 2017 Telerik AD. All rights reserved.                                                                                                                                                      
  *                                                                                                                                                                                                      
  * Kendo UI commercial licenses may be obtained at                                                                                                                                                      
