d552f55aafb59b18152bd1c2d79f296105c3bf62
nnava <lill4ekonomen@gmail.com>
2017-09-09 21:16:23 +0200

    Correction on loading Land

--- js/alasqlstockdata.js
+++ js/alasqlstockdata.js
@@ -20,7 +20,7 @@ define([], function() {
     function loadDataFromFileToTable() {
         alasql('TRUNCATE TABLE StockData');
         alasql("SELECT * FROM JSON('stockdata.json')",[],function(jsonResult){
-            alasql('INSERT INTO StockData SELECT N AS namn, K AS kortnamn, Y AS yahoosymbol, LI AS azalink, I AS isin, M AS marknad, CA AS bransch, CU AS handlas, LA = land FROM ?', [jsonResult]);
+            alasql('INSERT INTO StockData SELECT N AS namn, K AS kortnamn, Y AS yahoosymbol, LI AS azalink, I AS isin, M AS marknad, CA AS bransch, CU AS handlas, LA AS land FROM ?', [jsonResult]);
         });
     };
 
